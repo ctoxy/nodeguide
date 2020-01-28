63a32fddec8973634bcb978740e179a1b50848c8
ctoxy <cedric.bouve@gmail.com>
2020-01-28 17:03:25 +0100

    ajout du model PRODUCT

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,5 @@
-const products = [];
+
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
     res.render('add-product', {
@@ -11,12 +12,13 @@ exports.getAddProduct = (req, res, next) => {
   };
 
 exports.postAddProducts = (req, res, next) => {
-    products.push({ title: req.body.title });
+    const product= new Product(req.body.title);
+    product.save();
     res.redirect('/');
   };
 
 exports.getProduct = (req, res, next) => {
-    
+    const products = Product.fetchAll();
     res.render('shop', {
       prods: products,
       pageTitle: 'Shop',
