e0c2111b96b27cc233ae4f1923463153329ef9e0
ctoxy <cedric.bouve@gmail.com>
2020-01-29 11:11:40 +0100

    using fs and callback to display products

--- models/product.js
+++ models/product.js
@@ -1,15 +1,47 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
 
 module.exports = class Product {
-    constructor(t) {
-        this.title = t;
-    }
+  constructor(t) {
+    this.title = t;
+  }
 
-    save() {
-        products.push(this);
-    }
+  save() {
+    /*sauvegarde de products dans un repertoire data nomme products.json */
 
-    static fetchAll() {
-        return products;
-    }
-}
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+    /* lecture du contenu via fs si il existe pas on le crer vide */
+
+    fs.readFile(p, (err, filecontent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(filecontent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    /*sauvegarde de products dans un repertoire data nomme products.json */
+
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (err, filecontent) => {
+      if (err) {
+        cb([]);
+      }
+      /* on parse car products .json est un tableau */
+      cb(JSON.parse(filecontent));
+    });
+  }
+};
