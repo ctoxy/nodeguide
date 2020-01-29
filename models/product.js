3e6bc03799ce2e06bb658078f43873068fdff4e4
ctoxy <cedric.bouve@gmail.com>
2020-01-29 11:24:06 +0100

    refactorisation de code

--- models/product.js
+++ models/product.js
@@ -1,26 +1,31 @@
 const fs = require("fs");
 const path = require("path");
 
+/*sauvegarde de products dans un repertoire data nomme products.json */
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+    /* on parse car products .json est un tableau */
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
 
   save() {
-    /*sauvegarde de products dans un repertoire data nomme products.json */
-
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      "data",
-      "products.json"
-    );
-    /* lecture du contenu via fs si il existe pas on le crer vide */
-
-    fs.readFile(p, (err, filecontent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(filecontent);
-      }
+    getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
@@ -29,19 +34,7 @@ module.exports = class Product {
   }
 
   static fetchAll(cb) {
-    /*sauvegarde de products dans un repertoire data nomme products.json */
-
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      "data",
-      "products.json"
-    );
-    fs.readFile(p, (err, filecontent) => {
-      if (err) {
-        cb([]);
-      }
-      /* on parse car products .json est un tableau */
-      cb(JSON.parse(filecontent));
-    });
+    getProductsFromFile(cb);
   }
 };
+
